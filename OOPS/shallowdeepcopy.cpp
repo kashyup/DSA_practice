fb04cb248f2b7eb57428658b79a8a323e37b4303
kashyup <rohitkashyap69489@gmail.com>
2021-07-23 22:29:45 +0530

    shallow_copy_deep_copy

---
+++ OOPS/shallowdeepcopy.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+ int age;
+ char *name;
+ public:
+ student(int age, char *name){
+     this->age=age;
+     //shallow copy
+     //this->name=name;
+     this->name=new char[strlen(name)+1];
+     strcpy(this->name,name);
+
+ }
+ void display(){
+     cout<<age<<" "<<name<<endl;
+ }
+
+};
+
+int main(){
+     char name[]="abcd";
+     student s1(20,name);
+     s1.display();
+     name[3]='e';
+     student s2(21,name);
+     s2.display();
+     s1.display();
+}
