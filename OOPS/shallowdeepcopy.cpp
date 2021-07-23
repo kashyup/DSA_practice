dc603f6f58bc5735d59c5d186801d737d6b5acad
kashyup <rohitkashyap69489@gmail.com>
2021-07-23 22:58:54 +0530

    copy_constructor

--- OOPS/shallowdeepcopy.cpp
+++ OOPS/shallowdeepcopy.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 class student{
  int age;
+ public:
  char *name;
  public:
  student(int age, char *name){
@@ -12,6 +13,12 @@ class student{
      strcpy(this->name,name);
 
  }
+ //copy constructor//
+  student(student const &s){
+      this->age=s.age;
+      this->name=new char[strlen(name)+1];
+      strcpy(this->name,s.name);
+  }
  void display(){
      cout<<age<<" "<<name<<endl;
  }
@@ -22,8 +29,14 @@ int main(){
      char name[]="abcd";
      student s1(20,name);
      s1.display();
-     name[3]='e';
+     /*name[3]='e';
      student s2(21,name);
+     s2.display();
+     s1.display();
+     */
+     
+     student s2(s1);
+     s2.name[0]= 'x';
      s2.display();
      s1.display();
 }
