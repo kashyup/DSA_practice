e380bc9ff936fd8c34177bf512ef6de591acfc62
kashyup <rohitkashyap69489@gmail.com>
2021-07-28 19:41:11 +0530

    length-of -linked-list

---
+++ LinkedList-1/length.cpp
@@ -0,0 +1,61 @@
+//length of the linked list//
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+int length(Node *head)
+{
+    int length=0;
+    while(head!=NULL){
+        length++;
+        head=head->next;
+    }
+    return length;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		cout << length(head) << endl;
+	}
+	return 0;
+}
