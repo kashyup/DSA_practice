22651b046e56bb1c845b2919019adc082996b1bc
kashyup <rohitkashyap69489@gmail.com>
2021-08-15 15:09:19 +0530

    print-level-wise

--- Trees/q1.cpp
+++ Trees/q1.cpp
@@ -64,6 +64,30 @@ void printTree(TreeNode<int>*root){
          printTree(root->children[i]);
      }
 }
+void PrintLevelwise(TreeNode<int>*root){
+   if(root==NULL){
+        return;
+    }
+    queue<TreeNode<int>*>pendingQ;
+    pendingQ.push(root);
+    
+    while(pendingQ.size()!=0){
+        cout<<root->data<<":";
+        TreeNode<int>*front=pendingQ.front();
+        pendingQ.pop();
+        for(int i=0;i<root->children.size();i++){
+            pendingQ.push(root->children[i]);
+            if(root->children.size()-1!=i){
+                cout<<root->children[i]->data<<",";
+            }
+            else{
+                cout<<root->children[i]->data;
+            }
+        }
+        root=pendingQ.front();
+        cout<<endl;
+    }
+}
 int main(){
     /*TreeNode<int>* root=new TreeNode<int>(1);
     TreeNode<int>* node1=new TreeNode<int>(2);
@@ -72,7 +96,8 @@ int main(){
     root->children.push_back(node2);*/
    /// TreeNode<int>*root=takeInput();
    TreeNode<int>*root=takeInputLevel();
-    printTree(root);
+    //printTree(root);
+    PrintLevelwise(root);
 
 
 }
