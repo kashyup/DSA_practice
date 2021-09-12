79a65e5ce08e8a15a5ca386ee0245720eada141f
kashyup <rohitkashyap69489@gmail.com>
2021-09-12 17:27:09 +0530

    121. Best Time to Buy and Sell Stock

---
+++ Leetcode/q28.cpp
@@ -0,0 +1,100 @@
+/*
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     
+        int maxm =INT_MIN;
+        
+        int profit = INT_MIN;
+        
+        
+        vector<int>res(prices.size(),INT_MIN);
+        
+        for( int i =prices.size()-1; i>=0; i--){
+            
+            if(maxm <=prices[i]){
+                
+                maxm = prices[i];
+                
+                res[i]=maxm;
+                
+            }
+            else{
+                
+                res[i] = maxm;
+                
+            }
+            
+        }
+        
+       for( int i = 0 ; i< prices.size(); i++){
+           
+         int  op = res[i]-prices[i];
+               
+           if(profit<op){
+               
+               profit = op;
+           }
+           
+       } 
+        
+        return profit;
+        
+        
+    }
+};
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int minsf = prices[0];
+        
+        int profit = 0;
+        
+        for( int i = 0 ; i< prices.size(); i++){
+         
+            
+            minsf = min(prices[i],minsf);
+            
+           int op= prices[i] - minsf;
+            
+            profit = max(profit,op);
+          
+             
+        }
+        
+        return profit;
+        
+    }
+};
