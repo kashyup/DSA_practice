a629673c7a5b34d59910e1871f03fa2f7d1ed857
kashyup <rohitkashyap69489@gmail.com>
2021-09-12 16:13:02 +0530

    Contains X

---
+++ Trees/q7.cpp
@@ -0,0 +1,52 @@
+/*
+Given a generic tree and an integer x, check if x is present in the given tree or not. Return true if x is present, return false otherwise.
+Input format :
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.  
+The following line contains an integer, that denotes the value of x.
+Output format :
+The first and only line of output contains true, if x is present and false, otherwise.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+10 3 20 30 40 2 40 50 0 0 0 0
+40 
+Sample Output 1 :
+true
+Sample Input 2 :
+10 3 20 30 40 2 40 50 0 0 0 0
+4 
+Sample Output 2:
+false
+
+*/
+
+bool isPresent(TreeNode<int>* root, int x) {
+    // Write your code here
+    
+    queue<TreeNode<int>*>pq;
+    
+    
+	pq.push(root);
+    
+    while(!pq.empty()){
+        
+        TreeNode<int>*front = pq.front();
+        
+        pq.pop();
+        
+        if(front->data == x){
+            
+            return true;
+        }
+        
+        for( int i = 0 ; i<front->children.size() ; i++){
+            
+            pq.push(front->children[i]);
+            
+        }
+        
+        
+    }
+    
+    return false;
+}
